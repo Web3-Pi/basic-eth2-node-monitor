d98d9da42de8e90838023ddd678dd22e0197126d
Robert Mordzon <robertmordzon@gmail.com>
2024-05-13 08:43:16 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-cd /opt/web3pi/basic-eth2-node-monitor
-source "venv/bin/activate"
-python3 -m pip install -r requirements.txt
-python3 -u nodemonitor.py -sn "${HOSTNAME}.local" d -db localhost
